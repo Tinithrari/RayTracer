f0937238a85f1a49772ba1224f413cb7f3830eec
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2016-01-19 22:56:59 +0100

    Ajout qqes tests pour les manip de vecteurs

---
+++ checktriplet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Pour connaître l'emplacement du script compare.sh
+MYPATH=$(dirname "$0")
+
+# java -Dapple.awt.UIElement=true -cp $MYPATH/veclib Check "$1"
+
+echo "Pas encore réalisé"
+
