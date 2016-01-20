6b23cdadfae370e3442e5bc057b0b03d3f6f004e
Tinithrari <xavier.heugue@gmail.com>
2016-01-20 09:56:44 +0100

    Visu compil

--- Ray_Tracer/compare.sh
+++ Ray_Tracer/compare.sh
@@ -1,6 +1,6 @@
 mydir=$(dirname "$0")
 pushd
 cd "$mydir"/Ray_Tracer
-mdtool build -c:Ray_Tracer.sln > /dev/null
+mdtool build -c:Ray_Tracer.sln
 popd
 mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2"
