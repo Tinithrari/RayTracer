42b6a24bc16f98b32a80b908b00dea57717f8264
Tinithrari <xavier.heugue@gmail.com>
2016-01-20 10:08:49 +0100

    Esssais

--- compare.sh
+++ compare.sh
@@ -3,6 +3,4 @@ pushd
 cd "$mydir"/Ray_Tracer
 mdtool build -c:Ray_Tracer.sln > /dev/null
 popd
-mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2" 2> error
-
-echo $error
+mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2" 2>&1
