a4131f1a4682320ed8b6ce7260a080bdd131f760
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-20 10:39:33 +0100

    correction script.

--- compare.sh
+++ compare.sh
@@ -1,6 +1,8 @@
 mydir=$(dirname "$0")
+CURRENT=`pwd`
 pushd
 cd "$mydir"/Ray_Tracer
 mdtool build -c:Ray_Tracer.sln > /dev/null
 popd
-mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2" 2>&1
+mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$CURRENT/$1" "$CURRENT/$2" 2>&1
+mv $mydir/Ray_Tracer/diff.png $CURRENT/diff.png 
