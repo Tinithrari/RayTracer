b12bc90ed70a28f5d57caccc9bbbca0a0b146562
Tinithrari <xavier.heugue@gmail.com>
2016-01-20 10:02:17 +0100

    Essais

--- Ray_Tracer/compare.sh
+++ Ray_Tracer/compare.sh
@@ -3,5 +3,4 @@ pushd
 cd "$mydir"/Ray_Tracer
 mdtool build -c:Ray_Tracer.sln
 popd
-pwd
 mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2"
--- compare.sh
+++ compare.sh
@@ -1,3 +1,6 @@
-cd Ray_Tracer/Ray_Tracer
-mdtool build -c:Ray_Tracer.sln > /dev/null
-mono bin/Ray_Tracer.exe "../../$1" "../../$2"
+mydir=$(dirname "$0")
+pushd
+cd "$mydir"/Ray_Tracer
+mdtool build -c:Ray_Tracer.sln
+popd
+mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2"
