275ecaa9fc52068f8ae50977dd8328537f402c63
Tinithrari <xavier.heugue@gmail.com>
2016-01-20 10:05:07 +0100

    Redirection sortie compilation

--- compare.sh
+++ compare.sh
@@ -1,6 +1,6 @@
 mydir=$(dirname "$0")
 pushd
 cd "$mydir"/Ray_Tracer
-mdtool build -c:Ray_Tracer.sln
+mdtool build -c:Ray_Tracer.sln > /dev/null
 popd
 mono $mydir/Ray_Tracer/Ray_Tracer/bin/Ray_Tracer.exe "$1" "$2"
