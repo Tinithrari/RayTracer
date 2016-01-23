f552174a91b7c208e2d91c5278560e0e870c2d52
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-23 16:58:19 +0100

    Targeting project

--- compare.sh
+++ compare.sh
@@ -2,7 +2,7 @@ mydir=$(dirname "$0")
 CURRENT=`pwd`
 pushd
 cd "$mydir"/LanceurRayon/
-mdtool build -c:LanceurRayon.sln
+mdtool build -p:LanceurRayon.Comparateur
 popd
 mono $mydir/LanceurRayon/LanceurRayon.Comparateur/bin/comparateur.exe "$CURRENT/$1" "$CURRENT/$2" 2>&1
 mv $mydir/LanceurRayon/diff.png $CURRENT/diff.png 
