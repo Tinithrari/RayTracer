340410ea752aa00ae99a9e25f6ef7b7e843f786a
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2016-01-23 19:19:34 +0100

    moved echo

--- checktriplet.sh
+++ checktriplet.sh
@@ -1,7 +1,8 @@
+echo "DLB special"
 mydir=$(dirname "$0")
 pushd
 cd "$mydir"/LanceurRayon/
 mdtool build -p:LanceurRayon.TestCalcul 2>&1
 popd
-echo "DLB special"
+
 mono $mydir/LanceurRayon/LanceurRayon.Calcul/bin/debug/triplet.exe "$1" 2>&1
