f3c201ef79b7d8c705b59559de9316d45842f238
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-23 16:23:04 +0100

    affichage compilation

--- compare.sh
+++ compare.sh
@@ -2,7 +2,7 @@ mydir=$(dirname "$0")
 CURRENT=`pwd`
 pushd
 cd "$mydir"/LanceurRayon/LanceurRayon.Comparateur
-mdtool build -p:LanceurRayon.Comparateur.csproj > /dev/null
+mdtool build -p:LanceurRayon.Comparateur.csproj
 popd
 mono $mydir/LanceurRayon/LanceurRayon.Comparateur/bin/comparateur.exe "$CURRENT/$1" "$CURRENT/$2" 2>&1
 mv $mydir/LanceurRayon/diff.png $CURRENT/diff.png 
