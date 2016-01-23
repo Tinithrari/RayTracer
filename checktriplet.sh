0d05c0254a5268ff519a088e9c3f059a5072dcba
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-23 17:52:29 +0100

    Correction path exec

--- checktriplet.sh
+++ checktriplet.sh
@@ -3,4 +3,4 @@ pushd
 cd "$mydir"/LanceurRayon/
 mdtool build -p:LanceurRayon.TestMath
 popd
-mono $mydir/LanceurRayon/LanceurRayon.Calcul/bin/triplet.exe "$1" 2>&1
+mono $mydir/LanceurRayon/LanceurRayon.Calcul/bin/debug/triplet.exe "$1" 2>&1
