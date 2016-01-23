aa203061ac0e5bb5c58e94f6be1796366f276ed3
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-23 18:10:47 +0100

    Changement compil

--- checktriplet.sh
+++ checktriplet.sh
@@ -1,6 +1,6 @@
 mydir=$(dirname "$0")
 pushd
 cd "$mydir"/LanceurRayon/
-mdtool build -p:LanceurRayon.TestMath 2>&1
+mdtool build -p:LanceurRayon.TestCalcul 2>&1
 popd
 mono $mydir/LanceurRayon/LanceurRayon.Calcul/bin/debug/triplet.exe "$1" 2>&1
