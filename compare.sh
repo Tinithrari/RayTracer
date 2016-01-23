2d8917e06626b7d9a0cf8c8c602e103ece9fc412
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-23 16:50:23 +0100

    Moins ls

--- compare.sh
+++ compare.sh
@@ -2,7 +2,6 @@ mydir=$(dirname "$0")
 CURRENT=`pwd`
 pushd
 cd "$mydir"/LanceurRayon/
-ls
 mdtool build -p:LanceurRayon.Comparateur.csproj
 popd
 mono $mydir/LanceurRayon/LanceurRayon.Comparateur/bin/comparateur.exe "$CURRENT/$1" "$CURRENT/$2" 2>&1
