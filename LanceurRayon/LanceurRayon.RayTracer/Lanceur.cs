de839e8285ff910ce5b22c9a894755c10fe28bb1
Tinithrari <xavier.heugue@gmail.com>
2016-01-27 09:24:06 +0100

    Création Lanceur

---
+++ LanceurRayon/LanceurRayon.RayTracer/Lanceur.cs
@@ -0,0 +1,29 @@
+﻿using LanceurRayon.Math;
+using LanceurRayon.Renderer;
+
+namespace LanceurRayon.RayTracer
+{
+    public class Lanceur
+    {
+        private Scene   _scene;
+        private Repere _repere;
+        
+        public Lanceur(Scene _scene)
+        {
+            Vec3 tmpU, tmpV, tmpW, inter;
+
+            this._scene = _scene;
+
+            inter = _scene.Camera.LookFrom.sub(_scene.Camera.LookAt);
+            tmpW = inter.mul(1 / inter.norm());
+
+            inter = _scene.Camera.Up.sub(tmpW);
+            tmpU = inter.mul(1 / inter.norm());
+
+            inter = tmpU.cross(tmpW);
+            tmpV = inter.mul(1 / inter.norm());
+
+            _repere = new Repere(tmpU, tmpV, tmpW);
+        }
+    }
+}
---
+++ LanceurRayon/LanceurRayon.RayTracer/Repere.cs
@@ -0,0 +1,42 @@
+﻿using LanceurRayon.Math;
+
+namespace LanceurRayon.RayTracer
+{
+    public class Repere
+    {
+        private Vec3 u;
+        private Vec3 v;
+        private Vec3 w;
+
+        public Repere(Vec3 u, Vec3 v, Vec3 w)
+        {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        public Vec3 U
+        {
+            get
+            {
+                return u;
+            }
+        }
+
+        public Vec3 V
+        {
+            get
+            {
+                return v;
+            }
+        }
+
+        public Vec3 W
+        {
+            get
+            {
+                return w;
+            }
+        }
+    }
+}
