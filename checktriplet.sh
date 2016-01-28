35fc68c74877f649fe967437a3604fbf53e7b7ad
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-28 12:17:01 +0100

    Remove compile step

--- checktriplet.sh
+++ checktriplet.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 mydir=$(dirname "$0")
-pushd
-cd "$mydir"/LanceurRayon/
-mdtool build -p:LanceurRayon.TestMath > /dev/null
-popd
 mono $mydir/LanceurRayon/LanceurRayon.TestMath/bin/triplet.exe "$1" 2>&1
