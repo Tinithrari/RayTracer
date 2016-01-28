6ef8afc0dbed9b7086a66601d124ec414f8f5d6c
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-28 12:17:32 +0100

    Remove compile step

--- checkscene.sh
+++ checkscene.sh
@@ -6,8 +6,4 @@ MYPATH=$(dirname "$0")
 # java -Dapple.awt.UIElement=true -cp $MYPATH/bin CheckScene "$1" 2>&1
 
 CURRENT=`pwd`
-pushd
-cd "$MYPATH"/LanceurRayon/
-mdtool build -p:LanceurRayon.Renderer 2>&1
-popd
 mono $MYPATH/LanceurRayon/LanceurRayon.Renderer/bin/renderer.exe "$CURRENT/$1" 2>&1
