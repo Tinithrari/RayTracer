f542275a60755fb893d76dcdf8cb96eeb94c1946
Heugue Xavier <xavier_heugue@ens.univ-artois.fr>
2016-01-28 12:16:24 +0100

    remove compilation from compare.sh

--- compare.sh
+++ compare.sh
@@ -1,8 +1,4 @@
 mydir=$(dirname "$0")
 CURRENT=`pwd`
-pushd
-cd "$mydir"/LanceurRayon/
-mdtool build -p:LanceurRayon.Comparateur > /dev/null
-popd
 mono $mydir/LanceurRayon/LanceurRayon.Comparateur/bin/comparateur.exe "$CURRENT/$1" "$CURRENT/$2" 2>&1
 mv $mydir/LanceurRayon/diff.png $CURRENT/diff.png 
