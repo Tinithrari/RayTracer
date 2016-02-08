098a7ada1d8638cbfe7636b90b9644c03fbb57a3
Tinithrari <xavier.heugue@gmail.com>
2016-02-08 21:42:33 +0100

    Modification génération executable

--- raytrace.sh
+++ raytrace.sh
@@ -6,5 +6,5 @@ CURRENT=`pwd`
 
 # java -Dapple.awt.UIElement=true -jar $MYPATH/dlbraytracer.jar $1 2>&1
 
-mono $MYPATH/LanceurRayon/LanceurRayon.Lanceur/bin/renderer.exe "$CURRENT/$1"
+mono $MYPATH/LanceurRayon/LanceurRayon.Lanceur/bin/LanceurRayon.Lanceur.exe "$CURRENT/$1"
 mv $MYPATH/LanceurRayon/$(echo $1 | cut -d. -f1).png $MYPATH/
