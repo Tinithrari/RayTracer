d5cdc8835966d766e034d69c6bd86b245aa2962d
Tinithrari <xavier.heugue@gmail.com>
2016-02-08 21:34:54 +0100

    Moving image file

--- raytrace.sh
+++ raytrace.sh
@@ -7,3 +7,4 @@ CURRENT=`pwd`
 # java -Dapple.awt.UIElement=true -jar $MYPATH/dlbraytracer.jar $1 2>&1
 
 mono $MYPATH/LanceurRayon/LanceurRayon.Lanceur/bin/renderer.exe "$CURRENT/$1"
+mv $MYPATH/LanceurRayon/$(echo $1 | cut -d. -f1) $CURRENT
