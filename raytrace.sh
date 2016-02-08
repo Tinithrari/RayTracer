678faef7c54c70cfe43039310a4471cf22fb4edf
Tinithrari <xavier.heugue@gmail.com>
2016-02-08 21:37:59 +0100

    Moving file to root project

--- raytrace.sh
+++ raytrace.sh
@@ -7,4 +7,4 @@ CURRENT=`pwd`
 # java -Dapple.awt.UIElement=true -jar $MYPATH/dlbraytracer.jar $1 2>&1
 
 mono $MYPATH/LanceurRayon/LanceurRayon.Lanceur/bin/renderer.exe "$CURRENT/$1"
-mv $MYPATH/LanceurRayon/$(echo $1 | cut -d. -f1).png $CURRENT/
+mv $MYPATH/LanceurRayon/$(echo $1 | cut -d. -f1).png $MYPATH/
