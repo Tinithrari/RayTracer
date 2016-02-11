b976ef3728cd63faf345aa1e8fda5320ec40b970
andrzew_warembourg <angel4725@sfr.fr>
2016-02-11 12:55:41 +0100

    oh mon dieu il a encore frappé :/

--- LanceurRayon/LanceurRayon.Comparateur/ImgUtility.cs
+++ LanceurRayon/LanceurRayon.Comparateur/ImgUtility.cs
@@ -7,27 +7,18 @@ namespace LanceurRayon.Comparateur
 {
 	public class ImgUtility
 	{
-
 		/// <summary>
-
 		/// Permet de comparer deux image pixels par pixels
-
-		/// </summary>
-			
-		/// <param name="f1">Le nom de fichier de l'image numéro 1</param>
-				
+		/// </summary>	
+		/// <param name="f1">Le nom de fichier de l'image numéro 1</param>		
 		/// <param name="f2">Le nom de fichier de l'image numéro 2</param>
-
-
+       
 		public static void  ImgComparator(String f1,String f2)
-		{
-		
-		
+        { 
 			int  x,y,nb_pixels_diff=0;
 
 			Color couleur_img1, couleur_img2,couleur_pixel_courant;
-
-		    Bitmap img1,img2;
+		    Bitmap img1,img2,img_diff;
 		   
 			//On s'assure que les fichiers passés en paramètre existent.
 			try{
@@ -39,9 +30,7 @@ namespace LanceurRayon.Comparateur
 				throw new FileNotFoundException ("Le fichier à l'emplacement " + e.Message + " est manquant!!!!");
 			}
 
-
-
-			Bitmap img_diff = new Bitmap (img1.Size.Width,img1.Size.Height);
+			img_diff = new Bitmap (img1.Size.Width,img1.Size.Height);
 
 			//On s'assure que les images ont la même dimension.
 			if(img1.Size != img2.Size) {
@@ -57,26 +46,20 @@ namespace LanceurRayon.Comparateur
 					couleur_img2 =img2.GetPixel(x,y);
 
 					//On effectue la différence des deux couleurs.
-					img_diff.SetPixel(x,y, Color.FromArgb( 
-					                                      Math.Abs(couleur_img1.R-couleur_img2.R),
+					img_diff.SetPixel(x,y, Color.FromArgb(Math.Abs(couleur_img1.R-couleur_img2.R),
 					                                      Math.Abs(couleur_img1.G-couleur_img2.G),
-					                                      Math.Abs(couleur_img1.B-couleur_img2.B)
-					                                      )
-					                  );
-
+					                                      Math.Abs(couleur_img1.B-couleur_img2.B)));
 
 					couleur_pixel_courant = img_diff.GetPixel (x, y);
 
 					//On vérfie si le pixel obtenu n'est pas noir signe que la couleur des deux images est différente.
 					if ( couleur_pixel_courant.A != 255 ||couleur_pixel_courant.R !=0   ||couleur_pixel_courant.G != 0  || couleur_pixel_courant.B != 0   )
-						nb_pixels_diff++;
-					
+						nb_pixels_diff++;	
 				}
 			}
 
 			//On sauvegarde l'image différentielle.
 			img_diff.Save("diff.png",ImageFormat.Png);
-
 
 			if(nb_pixels_diff>=1000)
 				Console.WriteLine("KO");
@@ -84,13 +67,8 @@ namespace LanceurRayon.Comparateur
 			else
 				Console.WriteLine("OK");
 
-
 			Console.WriteLine(nb_pixels_diff);
-
-
 			return ;
-		
-		
 		}
 	}
 }
