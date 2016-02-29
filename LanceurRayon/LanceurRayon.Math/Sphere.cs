e0cc39393694a115e90ca076eee456fb2b909953
Tinithrari <xavier.heugue@gmail.com>
2016-02-29 20:15:55 +0100

    Modif borne inferieur bis

--- LanceurRayon/LanceurRayon.Math/Sphere.cs
+++ LanceurRayon/LanceurRayon.Math/Sphere.cs
@@ -58,9 +58,9 @@ namespace LanceurRayon.Math
 
             delta = (b * b) - (4 * a * c);
 
-            if (delta < - 0.00001d)
+            if (delta < - 0.00005d)
                 return null;
-            else if (delta >= -0.00001d && delta <= 0.00001d)
+            else if (delta >= -0.00005d && delta <= 0.00005d)
                 if (-b / (2 * a) > 0)
                     return -b / (2 * a);
                 else
