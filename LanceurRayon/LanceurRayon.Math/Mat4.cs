61ece07c7dd646f2f3af96cbcc06b3fb96593895
andrzew_warembourg <angel4725@sfr.fr>
2016-03-13 19:46:13 +0100

    Ajout massif de documentation sur Vec4 et Mat4.

--- LanceurRayon/LanceurRayon.Math/Mat4.cs
+++ LanceurRayon/LanceurRayon.Math/Mat4.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Text;
 
 namespace LanceurRayon.Math
-{
+{   /// <summary>
+    ///Classe modélisant une matrice carrée 4*4. 
+    /// </summary>
      public class Mat4
     {
+        /// <summary>
+        /// 1ere colonne de la matrice.
+        /// </summary>
         public Vec4 C1 { get;  set; }
+        
+        /// <summary>
+        /// 2nd colonne de la matrice.
+        /// </summary>
         public Vec4 C2 { get; set; }
+
+        /// <summary>
+        /// 3eme colonne de la matrice.
+        /// </summary>
         public Vec4 C3 { get; set; }
+
+        /// <summary>
+        /// 4eme colonne de la matrice.
+        /// </summary>
         public Vec4 C4 { get; set; }
 
+        /// <summary>
+        /// Constructeur de la classe.
+        /// </summary>
+        /// <param name="C1">1ere colonne de la matrice.</param>
+        /// <param name="C2">2nd colonne de la matrice.</param>
+        /// <param name="C3">3eme colonne de la matrice.</param>
+        /// <param name="C4">4eme colonne de la matrice.</param>
         public Mat4(Vec4 C1, Vec4 C2, Vec4 C3, Vec4 C4)
         {
             this.C1 = C1;
@@ -20,6 +44,11 @@ namespace LanceurRayon.Math
             this.C4 = C4;
         }
 
+        /// <summary>
+        /// Addition de 2 matrices.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns>Résultat de l'opération</returns>
         public Mat4 add(Mat4 m)
         {
 
@@ -27,11 +56,20 @@ namespace LanceurRayon.Math
 
         }
 
+        /// <summary>
+        /// Soustraction de 2 matrices.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns>Résultat de l'opération</returns>
         public Mat4 sub(Mat4 m)
         {
             return new Mat4(this.C1.sub(m.C1), this.C2.sub(m.C2), this.C3.sub(m.C3), this.C4.sub(m.C4));
         }
 
+        /// <summary>
+        /// Transposition de la matrice.
+        /// </summary>
+        /// <returns>Résultat de l'opération</returns>
         public Mat4 transpose()
         {
 
@@ -42,7 +80,12 @@ namespace LanceurRayon.Math
                            );
         }
 
-
+        /// <summary>
+        /// Créer la matrice de rotation d'angle t et d'axe v.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="angle"></param>
+        /// <returns>Résultat de l'opération</returns>
         public static Mat4 CreateRotationMatrix(Vec3 v,double angle)
         {
 
@@ -55,7 +98,11 @@ namespace LanceurRayon.Math
                             );
         }
 
-
+        /// <summary>
+        /// Créer la matrice de translation de vecteur V.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns>Résultat de l'opération</returns>
         public static Mat4 CreateTranslationMatrix(Vec3 v)
         {
 
@@ -66,6 +113,11 @@ namespace LanceurRayon.Math
                             );
         }
 
+        /// <summary>
+        /// Créer la matrice d'homotéthie selon le vecteur v.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns>Résultat de l'opération</returns>
         public static Mat4 CreateScalingMatrix(Vec3 v)
         {
 
--- LanceurRayon/LanceurRayon.Math/Vec4.cs
+++ LanceurRayon/LanceurRayon.Math/Vec4.cs
@@ -4,23 +4,44 @@ using System.Linq;
 using System.Text;
 
 namespace LanceurRayon.Math
-{
+{   /// <summary>
+    /// Vecteur de dimension quatre.
+    /// </summary>
     public class Vec4 : Vec3
     {
-
+        /// <summary>
+        /// Quatrième coordonnée du vecteur
+        /// </summary>
         public double T { get; set; }
 
+        /// <summary>
+        /// Constructeur de la classe.
+        /// </summary>
+        /// <param name="x">Abscice</param>
+        /// <param name="y">Ordonnée</param>
+        /// <param name="z">Côte</param>
+        /// <param name="T">4 ème coordonnée</param>
         public Vec4(double x, double y, double z, double T) :base(x,y,z){
 
             this.T = T;
         }
 
+        /// <summary>
+        /// Addition de deux vecteurs.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns>Vec4 résultant de l'opération</returns>
         public Vec4 add(Vec4 v) {
             if (v == null)
                 throw new ArgumentNullException();
             return new Vec4((double)(this.X + v.X), (double)(this.Y + v.Y), (double)(this.Z + v.Z), (double)(this.T + v.T));
         }
 
+        /// <summary>
+        /// Soustraction de 2 vecteurs
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns>Vec4 résultant de l'opération</returns>
         public Vec4 sub(Vec4 v)
         {
             if (v == null)
