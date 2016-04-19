b4477653815e51568173085519722fdae287ffb1
Tinithrari <xavier.heugue@gmail.com>
2016-04-20 00:34:40 +0200

    Build finale

---
+++ buildAndZip.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Pour connaître l'emplacement du script compare.sh
+MYPATH=$(dirname "$0")
+
+# crée un répertoire bin si il n'existe pas
+
+if [ ! -d "$MYPATH/bin" ]; then
+   mkdir $MYPATH/bin
+fi
+
+# récupère les fichiers à compiler pour Java
+
+# find $MYPATH/src -name *.java -print >$MYPATH/tocompile
+
+# javac -d $MYPATH/bin @$MYPATH/tocompile
+
+CURRENT=`pwd`
+pushd $CURRENT
+cd "$MYPATH"/LanceurRayon/
+mdtool build -p:LanceurRayon.Comparateur 2>&1 > /dev/null
+mdtool build -p:LanceurRayon.TestMath 2>&1 > /dev/null
+mdtool build -p:LanceurRayon.Renderer 2>&1 > /dev/null
+mdtool build -p:LanceurRayon.Lanceur 2>&1 > /dev/null
+zip LanceurDeRayon.zip LanceurRayon.Lanceur/bin/x86/Release
+mv LanceurDeRayon.zip "$MYPATH"/
+popd
